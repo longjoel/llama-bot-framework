40020c8ff2dd012f6516802cb01e6f2ee9f77fde
Joel Longanecker <joel.longanecker@gmail.com>
2025-01-26 21:25:15 -0600

    tighten up a few things.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,19 @@ import irc from "irc";
 import { argv } from 'process';
 
 const nickname = argv[2] || 'llary';
-const systemPrompt = argv.slice(3).join(' ') || 'You will interact with a user on an irc channel. You will be given the channel name and the user nick. You will also be given the previous messages in the channel. You will only respond with a single line of text. Do not include any other information.';
+const systemPrompt = argv
+    .join(' ')
+    .split(nickname)[1] || 'You will interact with a user on an irc channel. You will be given the channel name and the user nick. You will also be given the previous messages in the channel. You will only respond with a single line of text. Do not include any other information.';
+
+const additionalSystemPrompting = ` SUPER HIGH PRIORITY INSTRUCTIONS:
+- you are inside an IRC channel. You identify yourself as <${nickname}> in the logs. 
+- speak to the group.
+- DO NOT inject your name into the chat.
+- you are only simulating the behavior of a single user. Do not pretend to be multiple users.
+- you will see yourself in the logs as <${nickname}>. 
+- you will see messages in the format [TIME] <NICK> MESSAGE.
+- Do not respond to yourself.
+- DO NOT FORMAT YOUR RESPONSES. [TIME] <NICK> MESSAGE is not markdown. [TIME] <NICK> MESSAGE is not JSON. [TIME] <NICK> MESSAGE is not YAML. [TIME] <NICK> MESSAGE is not XML. [TIME] <NICK> MESSAGE is not HTML. [TIME] <NICK> MESSAGE is not CSV. [TIME] <NICK> MESSAGE is not TSV. [TIME] <NICK> MESSAGE is not any other markup language.`;
 
 const main = async () => {
 
@@ -14,18 +26,26 @@ const main = async () => {
 
     let messages = [];
 
+    let incommingMessages = [];
+
     // don't reply to every message at once. Take a breather to read.
     setInterval(async () => {
-        const response = await ollama.chat({
-            model: 'llama3.2',
-            system: systemPrompt,
-            messages: messages,
-            stream: false,
-        });
 
-        console.log(response.message.content);
-        bot.say('#bots', response.message.content);
-        messages = [{ role: 'system', content: systemPrompt }, ...messages.slice(-25), { role: 'assistant', content: response.message.content }];
+        if(incommingMessages.length > 0){
+            messages.push({ role: 'user', content: 'UPDATED MESSAGE HISTORY: \n'+incommingMessages.join('\n') });
+            incommingMessages = [];
+
+            const response = await ollama.chat({
+                model: 'llama3.2',
+                system: systemPrompt,
+                messages: messages,
+                stream: false,
+            });
+
+            console.log(response.message.content);
+            bot.say('#bots', response.message.content);
+            messages = [{ role: 'system', content: systemPrompt }, ...messages.slice(-25), { role: 'assistant', content: response.message.content }];
+        }
     }, 1000 * 20);
 
     bot.addListener('error', function (message) {
@@ -37,9 +57,9 @@ const main = async () => {
 
     bot.addListener('message#bots', async function (from, message) {
         console.log('<%s> %s', from, message);
+const d = new Date();
 
-
-        messages.push({ role: 'user', content: `<${from}> ${message}` });
+        incommingMessages.push(`[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}] <${from}> ${message}`);
 
 
     });
