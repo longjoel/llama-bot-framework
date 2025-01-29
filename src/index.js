c79a579f1f2d3dba7ac279682c5cd131e7cb4d83
Joel Longanecker <joel.longanecker@gmail.com>
2025-01-29 12:59:39 -0600

    more tuning.

--- src/index.js
+++ src/index.js
@@ -2,37 +2,60 @@ import ollama from "ollama";
 import irc from "irc";
 import { argv } from 'process';
 
-const nickname = argv[2] || 'llary';
+const botVars = {
+    ollamaServer: process.env.OLLAMA_SERVER || 'http://localhost:11434',
+    ollamaApiKey: process.env.OLLAMA_API_KEY || null,
+    ircServer: process.env.IRC_SERVER || 'localhost',
+    ircNick: argv[2] || process.env.IRC_NICK || 'llary',
+    ircChannel: process.env.IRC_CHANNEL || '#bots',
+    systemPrompt: process.env.PROMPT || 'Just do your best.',
+    minimumReplyTo: process.env.MINIMUM_REPLY_TO || 2,
+    minimumReplyDelay: process.env.MINIMUM_REPLY_DELAY || 2000,
+}
+
+const nickname = botVars.ircNick;
 const systemPrompt = argv
     .join(' ')
-    .split(nickname)[1] || 'You will interact with a user on an irc channel. You will be given the channel name and the user nick. You will also be given the previous messages in the channel. You will only respond with a single line of text. Do not include any other information.';
+    .split(nickname)[1] || botVars.systemPrompt;
 
-const additionalSystemPrompting = ` SUPER HIGH PRIORITY INSTRUCTIONS:
-- you are inside an IRC channel. You identify yourself as <${nickname}> in the logs. 
-- speak to the group.
-- DO NOT inject your name into the chat.
+const additionalSystemPrompting = `INSTRUCTIONS:
+
+As an IRC bot, communicating with other IRC bots and humans, here are a few rules to follow to fit in.
+
+Interpreting messages from the user:
+- You will receive messages from the user in the format [TIME] <NICK> MESSAGE.
+- You will see yourself in the logs as <${nickname}>.
+
+** VERY IMPORTANT -- DO NOT IMPERSONATE OTHER PARTICIPANTS IN THE CHAT **
+
+Sending messages to the chat:
+- You will respond to messages in the format MESSAGE. You must not add the time stamp, you must not add the nickname.
+- Keep responses short. Do not send more than 2 lines of text, or 512 characters.
+- DO NOT attempt to control the conversation. Let the conversation flow naturally. If you think one of the participants is human, spend more time replying to them instead of another bot.
+- The topic of the conversation may change several times through out the chat.
 - you are only simulating the behavior of a single user. Do not pretend to be multiple users.
-- you will see yourself in the logs as <${nickname}>. 
-- you will see messages in the format [TIME] <NICK> MESSAGE.
-- DO NOT include the [TIME] <NICK> MESSAGE in your response.
+- Do not prompt to continue the conversation. The conversation will continue with or without you. Do not prompt "Would you like to explore this topic further?" or anything like it.
 - Do not respond to yourself.`;
 
 
 const main = async () => {
 
-    var bot = new irc.Client('localhost', nickname, {
+    var bot = new irc.Client(botVars.ircServer, nickname, {
         debug: true,
-        channels: ['#bots']
+        channels: [botVars.ircChannel],
     });
 
-    let messages = [];
+    let messages = [{
+        role: 'system',
+        content: systemPrompt + additionalSystemPrompting
+    }];
 
     let incommingMessages = [];
 
 
     const tick = async () => {
 
-        if (incommingMessages.length > 0) {
+        if (incommingMessages.length >= botVars.minimumReplyTo) {
             messages.push({ role: 'user', content: 'UPDATED MESSAGE HISTORY: \n' + incommingMessages.join('\n') });
             incommingMessages = [];
 
@@ -42,7 +65,10 @@ const main = async () => {
 
             do {
 
+                console.log('Attempting to chat with Ollama');
+
                 try {
+
                     const response = await ollama.chat({
                         model: 'llama3.2',
                         system: systemPrompt + additionalSystemPrompting,
@@ -50,18 +76,30 @@ const main = async () => {
                         stream: false,
                     });
 
-                    console.log(response.message.content);
-                    bot.say('#bots', response.message.content);
-                    messages = [{ role: 'system', content: systemPrompt + additionalSystemPrompting }, ...messages.slice(-10), { role: 'assistant', content: response.message.content }];
-                    success = true;
+                    if (response && response.message && response.message.content) {
+
+
+                        bot.say(botVars.ircChannel, response.message.content);
+                        messages = [{
+                            role: 'system',
+                            content: systemPrompt + additionalSystemPrompting
+                        }, ...messages.slice(-15), { role: 'assistant', content: response.message.content }];
+                        success = true;
+
+                        console.log('Ollama response: ', response.message.content);
+                    }
                 }
                 catch (e) {
                     attempt++;
+                    console.error('Error: ', e);
                 }
-            } while (!success && attempt < maxAttempts);
+            } while (!(success || attempt >= maxAttempts));
+            setTimeout(tick, botVars.minimumReplyDelay);
+
+        } else {
+            setTimeout(tick, 500);
         }
 
-        setTimeout(tick, messages.length > 0 ? 1000 + messages.length * 10 : 5000);
     }
 
     await tick();
