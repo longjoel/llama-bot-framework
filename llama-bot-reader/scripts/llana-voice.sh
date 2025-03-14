b159d8f27daa028b62c9990dbca71e996b9368ac
joel longanecker <joel.longanecker@gmail.com>
2025-03-13 23:01:43 -0500

    start from scratch.

--- llama-bot-reader/scripts/llana-voice.sh
+++
@@ -1,4 +0,0 @@
-speak-ng -v en-french-4 -s 75 -p 80 "the quick brown fox jumps over the lazy dog"
-speak-ng -v en-french-4 -s 75 -p 80 "my name is llana, it's a pleasure to meet you"
-speak-ng -v en-french-4 -s 75 -p 80 "I am a computer program, I am here to help you"
-
