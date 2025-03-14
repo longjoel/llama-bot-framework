a5e8891922bc15ae7d041f0c0d5074cb208cf8b2
joel longanecker <joel.longanecker@gmail.com>
2025-03-13 23:55:48 -0500

    eh

---
+++ main.ts
@@ -0,0 +1,6 @@
+import {add} from './packages/llama-irc-daemon/main.ts';
+import {subtract} from './packages/llama-irc-daemon/main.ts';
+
+console.log("Add 2 + 3 =", add(2, 3));
+console.log("Add 2 + 3 =", subtract(6666, 3));
+
